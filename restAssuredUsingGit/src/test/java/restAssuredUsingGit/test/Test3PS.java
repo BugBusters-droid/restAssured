d1f57d44f6f28570e2e03fb99d615440ed53d46b
BugBusters-droid <venkatesh.n@qapitol.com>
2019-07-31 18:34:35 +0530

    jh

--- restAssuredUsingGit/src/test/java/restAssuredUsingGit/test/Test3PS.java
+++ restAssuredUsingGit/src/test/java/restAssuredUsingGit/test/Test3PS.java
@@ -15,7 +15,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import junit.framework.Assert;
 
-public class Test2PS {
+public class Test3PS {
 	
 	
     //GETCall   //To get all employees detail
